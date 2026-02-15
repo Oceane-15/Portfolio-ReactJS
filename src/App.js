fa90746af2a248e08388e7a126b163053deeafe0
Océane <docasaloceane05@gmail.com>
2026-02-15 14:11:30 +0100

    Router fait

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React from "react";
+import {Routes, Route} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<h1>Page d'accueil</h1>}/>
+        <Route path="/services" element={<h1>Services</h1>}/>
+        <Route path="/realisations" element={<h1>Réalisations</h1>}/>
+        <Route path="/contact" element={<h1>Contact</h1>}/>
+        <Route path="/mentions-legales" element={<h1>Mentions Légales</h1>}/>
+      </Routes>
     </div>
-  );
+  )
 }
 
 export default App;
