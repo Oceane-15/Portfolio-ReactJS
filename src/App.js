cb10ab4f8b1fdcf4dc373867f6f10cffa0659a38
Océane <docasaloceane05@gmail.com>
2026-02-16 17:17:07 +0100

    changement de chemin header

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import {Routes, Route} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Header from "./components/Header";
+import Header from "./components/header";
 
 
 
