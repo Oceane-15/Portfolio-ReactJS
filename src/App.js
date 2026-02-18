71acf5004000d977c1f7e7acf8b8650ff201c4f3
Océane <docasaloceane05@gmail.com>
2026-02-18 16:35:55 +0100

    Page Service ajoutée

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import Header from "./components/header";
 import Footer from "./components/footer";
 import Home from "./pages/home";
+import Services from './pages/services';
 
 
 
@@ -15,7 +16,7 @@ function App() {
      <main>
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/services" element={<h1>Services</h1>}/>
+        <Route path="/services" element={<Services/>}/>
         <Route path="/realisations" element={<h1>Réalisations</h1>}/>
         <Route path="/contact" element={<h1>Contact</h1>}/>
         <Route path="/mentions-legales" element={<h1>Mentions Légales</h1>}/>
