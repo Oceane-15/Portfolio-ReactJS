dc7beae7cb952354a2b5b817fad9863b67545a49
Océane <docasaloceane05@gmail.com>
2026-02-23 18:42:44 +0100

    Changement import header

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import {Routes, Route} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Header from "./components/header";
+import Header from "./components/Header";
 import Footer from "./components/footer";
 import Home from "./pages/home";
 import Services from "./pages/services";
